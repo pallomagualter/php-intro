3e6bf6fbd873dca11f706aa8732831a548227ee6
pallomagualter <pallomagualter@gmail.com>
2021-12-13 10:35:59 -0300

    session

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Formulário de Inscrição</title>
@@ -14,7 +18,7 @@
     <form action="script.php" method="post">
         <p>Seu nome: <input type="text" name="nome" /></p>
         <p>Sua idade: <input type="text" name="idade" /></p>
-        <p><input type="submit" value="Enviar dados do competidor"/></p>
+        <p><input type="submit" value="Enviar dados do competidor" /></p>
     </form>
 
 </body>
